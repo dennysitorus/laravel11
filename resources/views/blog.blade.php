37bcbe05d1a8264855727e32a7d013c7f14b2deb
Denny Sitorus <surel.denny@gmail.com>
2024-10-14 15:50:56 +0700

    Done ep 1

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Blog</title>
+</head>
+<body>
+    <h1>Halaman Blog</h1>
+    <article>
+        <h3>Judul 1</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos fuga et, perspiciatis totam, facilis dolor quisquam similique accusamus doloremque omnis corporis porro placeat incidunt laudantium vero dolore quas at quidem vel labore! Quis fugiat repellat aliquam. Quos, repellendus sit voluptatibus commodi quisquam voluptates. Quis, ratione magni expedita iusto quos odit veritatis delectus iure similique vero magnam hic assumenda sint enim obcaecati quam quia maiores consequatur fugiat officiis. Non rerum ab repellat quisquam ea dicta, modi a tempora molestiae voluptas ullam magni tempore, veritatis amet enim doloremque, sit qui. In eveniet iste magni modi beatae, similique minima veritatis sit laborum illo ratione eius, soluta assumenda aliquam. Blanditiis quibusdam dolorem libero debitis fugit itaque doloribus minus? At recusandae ipsum libero enim illo aliquam deleniti voluptatum eius praesentium quia et temporibus perferendis ad voluptatibus officiis, voluptates incidunt alias unde similique modi odit rerum veritatis ratione. Eligendi, expedita. Ipsum, sapiente maiores culpa explicabo autem eum sequi vitae quaerat, repellendus omnis delectus, aspernatur officiis. Maxime, voluptates voluptas? Officiis aliquid tempora vel quo! Dolore quis voluptate autem non omnis. Quia beatae maiores facere earum aliquid rem voluptatum deleniti eos fuga iusto tempora cupiditate expedita id unde tenetur omnis ut doloremque ab facilis corrupti, dolorum delectus autem.</p>
+    </article>
+
+    <article>
+        <h3>Judul 2</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro doloremque inventore atque quibusdam, cum repudiandae expedita pariatur minima in qui aut tenetur! Ex cupiditate asperiores adipisci debitis sed ab odit id, recusandae odio, velit repellendus quaerat eveniet voluptatem sequi nobis ullam harum eligendi blanditiis quas iste minus. Corrupti quo commodi quas expedita et? Rem laudantium numquam illo necessitatibus cumque quia aperiam molestias amet iste ad eius maiores soluta deserunt excepturi, nulla voluptatem. A, veritatis! Itaque quae nam maxime, quis accusamus numquam illum assumenda dolore vero laboriosam accusantium nihil ad maiores voluptatibus alias sequi dolorem minima pariatur possimus voluptate necessitatibus? Numquam animi nobis architecto veritatis tenetur ullam aspernatur dolores odit autem magni, adipisci asperiores! Est deserunt rerum illum minus enim sit delectus, minima et ipsam, incidunt, hic asperiores accusantium. Mollitia eius, impedit quis ullam adipisci voluptas eveniet nobis voluptatum nostrum odio excepturi ipsa repudiandae fugiat. Debitis eveniet, iste ex repudiandae et nisi laboriosam aspernatur temporibus tenetur aperiam perferendis quibusdam, nemo ipsa alias sequi delectus dolores eos optio assumenda odit iure sit porro unde fugit! Debitis, soluta voluptas. Praesentium eos nobis ea voluptas? Architecto, reprehenderit natus! Eius facilis adipisci quasi animi at corporis maiores in impedit. Optio voluptate delectus consequatur nam? Quis quia at officia iste itaque dolor quisquam iure accusamus excepturi quasi corporis laudantium nulla assumenda eos asperiores, dolorem amet voluptatibus vero repudiandae sed! Consequuntur numquam facere dolores cum architecto, officiis repudiandae. Eum enim rerum cum iure commodi laborum quia? Ex vitae similique nulla quis magni culpa asperiores magnam, vero laborum sapiente sunt ea eligendi, accusantium itaque mollitia deserunt autem beatae quo, reiciendis alias voluptas. Soluta assumenda, temporibus placeat libero accusamus quis praesentium rem, a obcaecati repudiandae tempore amet rerum! Laudantium est quia ab facilis itaque nemo repellendus vero quae suscipit delectus odit eaque, harum earum debitis veniam asperiores exercitationem veritatis.</p>
+    </article>
+</body>
+</html>
